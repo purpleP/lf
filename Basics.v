9b732a356515fc553b01c2a62acff8e7fdaa291e
Michael Doronin <warrior2031@mail.ru>
2019-04-08 08:35:54 +0300

    identity_fn_applied_twice

--- Basics.v
+++ Basics.v
@@ -1206,7 +1206,7 @@ Theorem identity_fn_applied_twice :
   (forall (x : bool), f x = x) ->
   forall (b : bool), f (f b) = b.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros f H b. rewrite -> H. rewrite -> H. reflexivity. Qed.
 
 (** Now state and prove a theorem [negation_fn_applied_twice] similar
     to the previous one but where the second hypothesis says that the
